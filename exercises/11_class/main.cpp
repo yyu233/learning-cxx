d950e4b40e234b80bbb6d1ceecfbe7774180f7e0
Yizheng Yu <yuyizheng@qiyuanlab.com>
2024-07-23 18:45:24 +0800

    ex 11

--- exercises/11_class/main.cpp
+++ exercises/11_class/main.cpp
@@ -14,11 +14,12 @@ class Fibonacci {
 
 public:
     // TODO: 实现构造器
-    // Fibonacci()
-
+    Fibonacci(): cache{}, cached(2) {
+       cache[1] = 1;
+    }
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (; false; ++cached) {
+        for (; cached <= i; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
