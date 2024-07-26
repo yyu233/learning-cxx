91bb006a48835c007d98e6f3478220fc03d1d756
Yizheng Yu <yuyizheng@qiyuanlab.com>
2024-07-26 20:59:19 +0800

    ex 29

--- exercises/29_std_accumulate/main.cpp
+++ exercises/29_std_accumulate/main.cpp
@@ -12,6 +12,7 @@ int main(int argc, char **argv) {
     //       - 连续存储；
     //       的张量占用的字节数
     // int size =
+    int size = std::accumulate(shape, shape + 4, 4, std::multiplies<int>());
     ASSERT(size == 602112, "4x1x3x224x224 = 602112");
     return 0;
 }
